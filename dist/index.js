5d2fb80f8bc66c1fdf6e1dbd9a6e5f0d1162a4c4
Alex Ellis (OpenFaaS Ltd) <alexellis2@gmail.com>
2020-11-02 15:02:13 +0000

    Update to return from data
    Signed-off-by: Alex Ellis (OpenFaaS Ltd) <alexellis2@gmail.com>

--- dist/index.js
+++ dist/index.js
@@ -5802,13 +5802,13 @@ class GetRelease {
     async getURL() {
         // Get owner and repo from context of payload that triggered the action
         const { owner, repo } = this.context.repo;
-        console.log(this.context.repo)
         // Get the tag name from the triggered action
         const tagName = this.context.ref;
     
         // This removes the 'refs/tags' portion of the string, i.e. from 'refs/tags/v1.10.15' to 'v1.10.15'
         const tag = tagName.replace("refs/tags/", "");
-    
+        console.log(this.context.repo, tag)
+
         // Get a release from the tag name
         // API Documentation: https://developer.github.com/v3/repos/releases/#create-a-release
         // Octokit Documentation: https://octokit.github.io/rest.js/#octokit-routes-repos-create-release
@@ -5817,28 +5817,22 @@ class GetRelease {
             repo,
             tag
         });
-
-        const {uploadUrl} = getReleaseResponse;
-        console.log(`Got release URL: '${uploadUrl}'`);
-        return uploadUrl;
+        const uploadURL = getReleaseResponse.data.upload_url
+        console.log(`Got release URL: '${uploadURL}'`);
+        return uploadURL;
     }
 }
 
 module.exports = GetRelease
 
-// const core = require('@actions/core');
 // const github = require('@actions/github');
-
-// const path = require('path');
-// const fs = require('fs');
-// const { pathToFileURL } = require('url');
 
 // async function runMe() {
 
 //     // Get authenticated GitHub client (Ocktokit): https://github.com/actions/toolkit/tree/master/packages/github#usage
-//     const octokit = github.getOctokit();
+//     const octokit = github.getOctokit("");
 //     const getRelease = new GetRelease(octokit, {
-//         ref: "refs/tags/0.1.14",
+//         ref: "refs/tags/0.1.19",
 //         repo: {
 //           repo: "release-it",
 //           owner:"alexellis",
